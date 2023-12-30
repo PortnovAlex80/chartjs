d2e8d859d53f7f094f6a29eaadb5a2cc6f2a3608
Aleksei <savinopoint@ya.ru>
2023-12-30 22:15:39 +0300

    +

--- public/ChartDataAggregator.js
+++ public/ChartDataAggregator.js
@@ -7,12 +7,12 @@ const generateChartData = require('../utils/generateChartData');
 function ChartDataAggregator() {
     const originalPoints = generatePolyline();
     const simplifiedPoints = rdpSimplifier(originalPoints, 0.25);
-    const cornerPoints = findCornerPoints(originalPoints, 0.50);
+    const leastSquaresLine = findCornerPoints(originalPoints, 0.50);
   
     // Добавляйте дополнительные секции здесь
     // ...
   
-    return generateChartData(originalPoints, simplifiedPoints, cornerPoints);
+    return generateChartData(originalPoints, simplifiedPoints, leastSquaresLine);
   }
 
   module.exports = ChartDataAggregator;
