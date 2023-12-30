db413ebe485972f080105fe43c62655ff9fd31dd
Aleksei <savinopoint@ya.ru>
2023-12-30 22:15:15 +0300

    LeastSquMethod add. one line

--- public/ChartDataAggregator.js
+++ public/ChartDataAggregator.js
@@ -1,21 +1,20 @@
 // /public/ChartDataAggregator.js
 const generatePolyline = require('../geodetic/polylineErrorGenerator');
-const rdpSimplifier = require('../filtres/rdpSimplifier');
-
+const rdpSimplifier = require('../filters/rdpSimplifier');
+const findCornerPoints = require('../filters/leastSquaresLine')
 const generateChartData = require('../utils/generateChartData');
 
 function ChartDataAggregator() {
     const originalPoints = generatePolyline();
     const simplifiedPoints = rdpSimplifier(originalPoints, 0.25);
+    const cornerPoints = findCornerPoints(originalPoints, 0.50);
   
     // Добавляйте дополнительные секции здесь
     // ...
   
-    return generateChartData(originalPoints, simplifiedPoints);
+    return generateChartData(originalPoints, simplifiedPoints, cornerPoints);
   }
-  
+
   module.exports = ChartDataAggregator;
   
-  
-  module.exports = ChartDataAggregator;
   
