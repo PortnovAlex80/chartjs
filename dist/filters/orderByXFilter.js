b7781661076f96424280dd8d5e0e283be167177e
PortnovAlex80 <46358607+PortnovAlex80@users.noreply.github.com>
2024-01-17 23:38:19 +0300

    Delete dist directory

--- dist/filters/orderByXFilter.js
+++
@@ -1,8 +0,0 @@
-const orderByXFilter = (points) => {
-    // Копируем массив, чтобы не изменять исходный
-    const pointsCopy = [...points];
-    // Сортируем точки по возрастанию координаты X
-    pointsCopy.sort((a, b) => a.x - b.x);
-    return pointsCopy;
-};
-export default orderByXFilter;
--- dist/sectiongenerators/errorGenerator.js
+++
@@ -1,8 +0,0 @@
-// src/utils/generateError.ts
-/**
- * Generates a random error value.
- * @returns A random number between -0.1 and 0.1.
- */
-export function generateError() {
-    return (Math.random() - 0.5) * 0.2;
-}
